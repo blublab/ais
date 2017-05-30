2b58d9264cc474b2de8d38da57df81c745a922d6
blublab <you@example.com>
2017-05-30 19:02:46 +0200

    final

--- ais-demo/src/main/DemoGUI.java
+++ ais-demo/src/main/DemoGUI.java
@@ -12,29 +12,30 @@ import org.neo4j.server.ServerBootstrapper;
 
 public class DemoGUI {
 
+	// Relationen definieren
 	enum Relationship implements RelationshipType {
 		NACHBAR, VOR
 	}
 
 	public static void main(String[] args) throws IOException {
 
-        ServerBootstrapper serverBootstrapper = new CommunityBootstrapper();
-
 		// Pfad zur Datenbank
 		File db_path = new File("./graph.db");
 
+		// Bootstrapeper für Browser GUI
+        ServerBootstrapper serverBootstrapper = new CommunityBootstrapper();
 		serverBootstrapper.start(
 				db_path,
-	            Optional.empty(), // omit configfile, properties follow
+	            Optional.empty(),
 	            Pair.of("dbms.connector.http.address","127.0.0.1:7575"),
 	            Pair.of("dbms.connector.http.enabled", "true"),
 	            Pair.of("dbms.connector.bolt.enabled", "true"),
 
-	            // allow the shell connections via port 1337 (default)
 	            Pair.of("dbms.shell.enabled", "true"),
 	            Pair.of("dbms.shell.host", "127.0.0.1"),
 	            Pair.of("dbms.shell.port", "1337")
 	        );
+		
 		// Instanziiere DB Service
         NeoServer neoServer = serverBootstrapper.getServer();
         GraphDatabaseService graphDB = neoServer.getDatabase().getGraph();
@@ -42,29 +43,31 @@ public class DemoGUI {
 		// Starte Transaktion
 		try (Transaction tx = graphDB.beginTx()) {
 			
-			// Erstelle Label "Student"
+			// Erstelle Label 
 			Label lStudent = Label.label("Student");
+			Label lProf = Label.label("Prof");
+
 			
 			// Erstelle Knoten 
 			Node node1 = graphDB.createNode(lStudent);
 			Node node2 = graphDB.createNode(lStudent);
-			Node node3 = graphDB.createNode(lStudent);
+			Node node3 = graphDB.createNode(lProf);
 
 			// Setzte Namen
-			node1.setProperty("name", "Peter");
-			node2.setProperty("name", "Andreas");
-			node3.setProperty("name", "Tom");
+			node1.setProperty("name", "Christof");
+			node2.setProperty("name", "KHB");
+			node3.setProperty("name", "Christian");
 
 			// Erstelle Beziehungen
 			node1.createRelationshipTo(node2, Relationship.NACHBAR);
-			node3.createRelationshipTo(node1, Relationship.VOR);
+			node1.createRelationshipTo(node3, Relationship.VOR);
 			
 			// Transaktion erfolgreich
 			tx.success();
 		}
 		
 
-        System.out.println("Press ENTER to quit.");
+        System.out.println("ENTER zum Beenden.");
         System.in.read();
         System.exit(0);
 	}
